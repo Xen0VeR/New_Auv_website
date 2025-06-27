55333583cce09e78f53b93d899790528086e3d15
Arush <arrush.narayan01@gmail.com>
2025-06-28 04:26:04 +0530

    server changes

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ const express=require("express");
 const path=require("path");
 
 const app=express()
+
+// Add security headers (CSP + COEP + COOP)
 app.use((req, res, next) => {
     res.set('Cross-Origin-Embedder-Policy', 'require-corp');
     res.set('Cross-Origin-Opener-Policy', 'same-origin');
+    res.set('Content-Security-Policy', 
+        "frame-src https://www.youtube.com https://www.youtube-nocookie.com https://sketchfab.com https://*.sketchfab.com;"
+    );
     next();
 });
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/game",express.static(path.join(__dirname, 'game')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'game', 'game.html'));
-// });
-
+// Start server
 app.listen(3000, ()=>{
     console.log("Sever is running")
 })
