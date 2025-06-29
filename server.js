4d9ff7c5658d7bed34c88380dde670ee0a56ba25
Arush <arrush.narayan01@gmail.com>
2025-06-30 02:46:30 +0530

    iframe issue sorted

--- server.js
+++ server.js
@@ -18,21 +18,24 @@ const app = express();
 //   next();
 // });
 
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, path, stat) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Content-Security-Policy',
-      "default-src 'self'; " +
-      "script-src 'self' https://static.sketchfab.com https://sketchfab.com; " +
-      "frame-src https://sketchfab.com https://*.sketchfab.com; " +
-      "connect-src https://sketchfab.com https://*.sketchfab.com; " +
-      "img-src 'self' https://sketchfab.com https://*.sketchfab.com data:; " +
-      "style-src 'self' 'unsafe-inline';"
-    );
-    res.set('Cross-Origin-Embedder-Policy', 'require-corp');
-    res.set('Cross-Origin-Opener-Policy', 'same-origin');
-  }
-}));
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    setHeaders: (res, path, stat) => {
+      res.set("Access-Control-Allow-Origin", "*");
+      res.set('Content-Security-Policy',
+        "default-src 'self'; " +
+        "script-src 'self' 'unsafe-inline' https://static.sketchfab.com https://sketchfab.com https://cdnjs.cloudflare.com; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://use.fontawesome.com; " +
+        "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com data:; " +
+        "frame-src https://sketchfab.com https://*.sketchfab.com https://www.youtube.com https://www.youtube-nocookie.com; " +
+        "connect-src 'self' http://localhost:3000 https://sketchfab.com https://*.sketchfab.com; " +
+        "img-src 'self' https://sketchfab.com https://*.sketchfab.com data:;"
+      );
+      // res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+      // res.set('Cross-Origin-Opener-Policy', 'same-origin');
+    },
+  })
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
